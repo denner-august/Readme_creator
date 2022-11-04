e57e247167342badb165e84ebfa0c38157380d44
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-11-04 10:06:50 -0300

    pagina de results

--- readme-creator/pages/results.tsx
+++ readme-creator/pages/results.tsx
@@ -1,14 +1,44 @@
 import Principal from "../components/principa";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Context } from "../context/Dados";
+
+import styles from "../styles/results.module.scss";
+
 export default function Results() {
   const { images, Dados } = useContext(Context);
 
+  console.log(Dados.products);
+
+  const [textoImage, setTextoImage] = useState<string[]>([]);
+  const texto =
+    '![texto alternativo](apaque e coloque a url da sua imagem aqui "titulo")';
+
+  useEffect(() => {
+    for (let index = 1; index <= images; index++) {
+      setTextoImage((preve) => (preve = [...preve, texto]));
+    }
+
+    return () => {
+      setTextoImage([]);
+    };
+  }, []);
+
   return (
     <Principal>
-      <h1>{images}</h1>
-      <h1>{Dados.titulo}</h1>
-      <h1>{Dados.descrição}</h1>
+      <div className={styles.Container}>
+        <h1> # {Dados.titulo}</h1>
+        <p># {Dados.descrição}</p>
+
+        {textoImage.map((value, index) => {
+          return (
+            <div key={index}>
+              <p>{value}</p>
+            </div>
+          );
+        })}
+
+        {}
+      </div>
     </Principal>
   );
 }
